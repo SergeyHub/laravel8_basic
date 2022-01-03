8f3a045620502bb3e175fb4b97e78d709a1016f6
Sergey <scoder127@gmail.com>
2022-01-03 14:39:28 +0500

    7 Eloquent ORM Read Users Data

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,5 +36,6 @@ Route::get('/about', function () {
 Route::get('/contact',[ContactController::class, 'index'])->name('contact_page');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    $users = User::all();
+    return view('dashboard',compact('users'));
 })->name('dashboard');
